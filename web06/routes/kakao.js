f0dd0fa05f506a3f6b4385656c1b88d4a873099c
Yun-SeYeong <dbstpdud09@naver.com>
2024-10-12 18:41:54 +0900

    6주차

--- web06/routes/kakao.js
+++ web06/routes/kakao.js
@@ -13,5 +13,24 @@ router.get('/blog', function(req, res, next) {
   res.render('index', { title: '블로그검색', pageName:'kakao/blog.ejs' });
 });
 
+router.get('/book/info', function(req, res, next) {
+  const title = req.query.title;
+  const thumbnail = req.query.thumbnail;
+  const price = req.query.price;
+  const authors = req.query.authors;
+  const publisher = req.query.publisher;
+  const isbn = req.query.isbn;
+  const contents = req.query.contents;
+  const info = {
+    title,
+    thumbnail,
+    price,
+    authors,
+    publisher,
+    isbn,
+    contents
+  }
+  res.render('index', { title: '도서정보 페이지', pageName:'kakao/info.ejs', info: info});
+});
 
 module.exports = router;
