760114736a8d17fa7fed16f17d861b795a0e26b9
Yun-SeYeong <dbstpdud09@naver.com>
2024-10-12 16:26:56 +0900

    홍길동 -> 윤세영

--- web04/routes/index.js
+++ web04/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: '홍길동 페이지', pageName:'home.ejs'});
+  res.render('index', {title: '윤세영 페이지', pageName:'home.ejs'});
 });
 
 module.exports = router;
